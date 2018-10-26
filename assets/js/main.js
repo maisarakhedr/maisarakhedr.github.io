80d5c4557ce28e9baa0c1ab40d1b1ea165bf276f
maisarakhedr <maisarakhedr@gmail.com>
2018-10-26 13:25:08 +0200

    Enable adding posts, adding layout and listing page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,18 @@
 jQuery(document).ready(function($) {
 
-    $('.level-bar-inner').css('width', '0');
-    
-    $(window).on('load', function() {
+  $('.level-bar-inner').css('width', '0');
+  $(window).on('load', function() {
 
-        $('.level-bar-inner').each(function() {
-        
-            var itemWidth = $(this).data('level');
-            
-            $(this).animate({
-                width: itemWidth
-            }, 800);
-            
-        });
-
+    $('.level-bar-inner').each(function() {
+      var itemWidth = $(this).data('level');
+      $(this).animate({
+        width: itemWidth
+      }, 800);
     });
-   
-    
-
+  });
+  $('a[href="Youtube-Link"]').each(function() {
+    $(this).replaceWith('<iframe width="560" height="315" src="' + 
+      youtube_links +
+      ' " frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>');
+  });
 });
